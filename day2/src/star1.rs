c772e64ff325d8ce360f3c8b9e07825b1f9f21ae
Jonatan Kruse <jonatankruse@hotmail.com>
2022-12-01 15:00:40 +0100

    prepp improvments

--- day2/src/star1.rs
+++ day2/src/star1.rs
@@ -1,11 +1,4 @@
-use std::{fs::File, io::Read};
-
 pub fn star1(){
-    let mut file = File::open("C:/Users/jonat/progProject/egnaProject/AdventOfRust/day2/src/in_test.txt")
-    .expect("File not found");
-    let mut data = String::new();
-    file.read_to_string(&mut data)
-    .expect("Error while reading file");
-
+    let data = include_str!("in_1.txt");
     println!("{}", data)
 }
